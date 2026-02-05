58c97bcf775e48c49b27236631c8fdb71e278f4f
Pratyush Poddar <pratyush.poddar@sequoia.com>
2026-02-05 17:34:20 +0530

    remove node_modules

--- node_modules/@rsbuild/core/dist-types/server/overlay.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function formatDisplayPath(filePath: string, isAbsolute: boolean, root?: string): string;
-export declare function convertLinksInHtml(text: string, root?: string): string;
-export declare function renderErrorToHtml(error: string, root?: string): string;
--- node_modules/@rsbuild/core/dist-types/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-export type * from './config';
-export type * from './context';
-export type * from './hooks';
-export type * from './plugin';
-export type * from './rsbuild';
-export type * from './rspack';
-export type * from './thirdParty';
-export type * from './utils';
